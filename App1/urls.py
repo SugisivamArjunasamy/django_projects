b459e6db8b68e91db4d405f1199a3aef5c1389c6
SugisivamArjunasamy <sugisivamarjunasamy2004@gmail.com>
2024-03-04 21:05:25 +0530

    web page for gamming accesseries

--- App1/urls.py
+++ App1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-from .views import home_view, playstation , monitor , joystick , keybord 
+from .views import home_view, playstation , monitor , joystick , keybord , monitorpage , monitor1 , monitor2 , monitor3
+from .views import joystickpage , joystick1 , joystick2 , joystick3
 
 urlpatterns =[
     path ("",home_view, name="home"),
@@ -7,4 +8,12 @@ urlpatterns =[
     path('monitor/', monitor, name='monitor'),
     path('joystick/', joystick, name='joystick'),
     path('keybord/', keybord, name='keybord'),
+    path('monitorpage/', monitorpage, name='monitorpage'),
+    path('monitor1/', monitor1, name='monitor1'),
+    path('monitor2/', monitor2, name='monitor2'),
+    path('monitor3/', monitor3, name='monitor3'),
+    path('joystickpage/', joystickpage, name='joystickpage'),
+    path('joystick1/', joystick1, name='joystick1'),
+    path('joystick2/', joystick2, name='joystick2'),
+    path('joystick3/', joystick3, name='joystick3'),
 ]
--- App1/views.py
+++ App1/views.py
@@ -11,3 +11,19 @@ def joystick(request):
     return render(request, 'joystick.html')
 def keybord(request):
     return render(request, 'keybord.html')
+def monitorpage(request):
+    return render(request, 'monitorpage.html')
+def monitor1(request):
+    return render(request, 'monitor1.html')
+def monitor2(request):
+    return render(request, 'monitor2.html')
+def monitor3(request):
+    return render(request, 'monitor3.html')
+def joystickpage(request):
+    return render(request, 'joystickpage.html')
+def joystick1(request):
+    return render(request, 'joystick1.html')
+def joystick2(request):
+    return render(request, 'joystick2.html')
+def joystick3(request):
+    return render(request, 'joystick3.html')
